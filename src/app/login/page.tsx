c8f7b52dd4ef8b051cffdf7d424f5eb3ea111dd2
itiaditi <aditi.tlgt244@gmail.com>
2024-07-12 21:30:06 +0530

    add

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -39,11 +39,7 @@ const Login = () => {
           onClick={handleGoogleLogin}
           className="rounded-lg border-2 p-2 sm:px-4 flex justify-center items-center mx-auto sm:gap-2"
         >
-          <Image
-            src="https://static-00.iconduck.com/assets.00/google-icon-2048x2048-pks9lbdv.png"
-            alt="google"
-            className="w-8 h-8 sm:w-10 sm:h-10"
-          />
+          
           <h2 className="text-sm sm:text-md font-medium sm:font-semibold">Sign up with Google</h2>
         </button>
       </div>
